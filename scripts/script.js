43c123bbd04cf09a86658a6d2f31958c6277c6ae
Yulia Zherebtsova <juliaz1206@yandex.ru>
2021-09-21 03:53:51 +0300

    feat: светлая тема (верстка)

--- scripts/script.js
+++ scripts/script.js
@@ -34,6 +34,32 @@ formApplyFooter.addEventListener('submit', (evt) => {
   }
 });
 
+// Select the button
+const toggle = document.querySelector(".toggle__checkbox");
+/*
+// Check for dark mode preference at the OS level
+const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+// Get the user's theme preference from local storage, if it's available
+const currentTheme = localStorage.getItem("theme");
+// If the user's preference in localStorage is dark...
+if (currentTheme == "dark") {
+  // ...let's toggle the .dark-theme class on the body
+  document.body.classList.toggle("dark-mode");
+// Otherwise, if the user's preference in localStorage is light...
+} else if (currentTheme == "light") {
+  // ...let's toggle the .light-theme class on the body
+  document.body.classList.toggle("light-mode");
+}*/
+
+// Listen for a click on the button
+toggle.addEventListener('change', (evt) => {
+  if (evt.target.checked) {
+    console.log("Checkbox is checked..");
+  } else {
+    console.log("Checkbox is not checked..");
+  }
+});
+
 // buttonDonate.addEventListener('click', (evt) => {
 //   // добавляем слушатель на кнопку поддержки
 //   alert(`just clicked the DONATE button!`);
